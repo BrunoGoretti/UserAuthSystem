aa33a9c57bb6b4e5ff26e7c3c599ff33fa4a9a36
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2024-08-07 16:48:24 +0400

    Add jsons

--- UserAuthSystemMvc/UserAuthSystemMvc/Services/EmailService.cs
+++ UserAuthSystemMvc/UserAuthSystemMvc/Services/EmailService.cs
@@ -1,5 +1,6 @@
 ﻿using MailKit.Net.Smtp;
 using MimeKit;
+using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
 using UserAuthSystemMvc.Services.Interfaces;
 
@@ -7,23 +8,30 @@ namespace UserAuthSystemMvc.Services
 {
     public class EmailService : IEmailService
     {
-        private readonly string _smtpServer = "smtp.gmail.com"; 
-        private readonly int _smtpPort = 587; 
-        private readonly string _emailAddress = "brunogorettiart@gmail.com";
-        private readonly string _emailPassword = "hvkd nfky zbkt acce"; 
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public async Task SendEmailAsync(string toEmail, string subject, string message)
         {
+            var smtpServer = _configuration["EmailSettings:SmtpServer"];
+            var smtpPort = int.Parse(_configuration["EmailSettings:SmtpPort"]);
+            var emailAddress = _configuration["EmailSettings:EmailAddress"];
+            var emailPassword = _configuration["EmailSettings:EmailPassword"];
+
             var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress("Your App Name", _emailAddress));
+            emailMessage.From.Add(new MailboxAddress("UserAuthSystem", emailAddress));
             emailMessage.To.Add(new MailboxAddress("", toEmail));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart("plain") { Text = message };
 
             using (var client = new SmtpClient())
             {
-                await client.ConnectAsync(_smtpServer, _smtpPort, MailKit.Security.SecureSocketOptions.StartTls);
-                await client.AuthenticateAsync(_emailAddress, _emailPassword);
+                await client.ConnectAsync(smtpServer, smtpPort, MailKit.Security.SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(emailAddress, emailPassword);
                 await client.SendAsync(emailMessage);
                 await client.DisconnectAsync(true);
             }
